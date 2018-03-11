b6df2b051c78403efc76363f24cd6caf5f699f38
Yusuf Simonson <simonson@gmail.com>
2018-03-11 19:13:55 +0800

    Just dockerize postgres

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM rust
-WORKDIR /app
-RUN apt-get update && apt-get install -y clang postgresql-contrib
-RUN rustup default stable
-CMD ./test.sh
--- lib/test.sh
+++ lib/test.sh
@@ -4,7 +4,7 @@ set -e
 
 export TEST_RDB_DIRECTORY=`mktemp -d 2>/dev/null || mktemp -d -t 'indradb'`
 export RUST_BACKTRACE=1
-export TEST_POSTGRES_URL="postgres://postgres@postgres:5432/indradb_test"
+export TEST_POSTGRES_URL="postgres://postgres@localhost:5432/indradb_test"
 
 ACTION=test
 
@@ -18,11 +18,13 @@ done
 function cleanup {
     echo "Cleaning up..."
     rm -r $TEST_RDB_DIRECTORY
+    docker-compose stop
 }
 
 mkdir -p $TEST_RDB_DIRECTORY
+docker-compose up -d
 trap cleanup EXIT
 
-dropdb -U postgres -h postgres --if-exists indradb_test
-createdb -U postgres -h postgres --owner=postgres indradb_test
+dropdb -U postgres -h localhost --if-exists indradb_test
+createdb -U postgres -h localhost --owner=postgres indradb_test
 cargo $ACTION --all-features $TEST_NAME
